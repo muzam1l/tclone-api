2b0ea0b9c28f624de6ffdf182933ffb4e64bda4c
muzam <muzamilsofi@outlook.com>
2020-06-08 15:38:12 +0530

    fixed mongo store warning

--- app.js
+++ app.js
@@ -35,10 +35,10 @@ app.use(session({
     name: 'tclone',
     resave: false,
     saveUninitialized: true,
-    // store: new MongoStore({
-    //     mongooseConnection: mongoose.connection,
-    //     //secret: 'this will encrypt my sessions'
-    // }),
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        //secret: 'this will encrypt my sessions'
+    }),
     cookie: {
         //secure: true,
         maxAge: 30 * 24 * 60 * 60 * 1000 //30 days
