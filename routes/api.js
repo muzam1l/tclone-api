913942eaab22d073a78c20967e10cb1332bdaa6d
muzam1l <muzamilsofi@outlook.com>
2022-04-09 01:04:14 +0530

    Fix routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express')
 const { ensureLoggedIn } = require('../utils/middlewares')
-
-const { createPost,
+const {
+    createPost,
     getPost,
     likePost,
     unlikePost,
@@ -12,25 +10,43 @@ const { createPost,
     getLikes,
     getReposts,
     replyToPost,
-    getReplies
+    getReplies,
 } = require('../controllers/post.controller')
-const { getUser, followUser, unFollowUser, updateUser, getFollowers, getFriends } = require('../controllers/user.controller')
+const {
+    getUser,
+    followUser,
+    unFollowUser,
+    updateUser,
+    getFollowers,
+    getFriends,
+} = require('../controllers/user.controller')
 const { homeTimeline, userTimeline } = require('../controllers/timeline.controller')
 const { search, trends, userSuggests } = require('../controllers/search.controller')
-const { notificationRead, getNotifications, subscribeDevice, unsubscribeDevice } = require('../controllers/notifications.controller')
+const {
+    notificationRead,
+    getNotifications,
+    subscribeDevice,
+    unsubscribeDevice,
+} = require('../controllers/notifications.controller')
 
-/* GET notification read*/
-router.get('/notification_read/:_id', ensureLoggedIn, notificationRead)
+const router = express.Router()
+
+/* POST read notification*/
+router.post('/notification_read/:_id', ensureLoggedIn, notificationRead)
+
 /* GET all notifications */
 router.get('/notifications', ensureLoggedIn, getNotifications)
+
 /* push subscribe, unsubscribe */
-router.post('/notifications/subscribe', ensureLoggedIn, subscribeDevice);
-router.get('/notifications/unsubscribe', ensureLoggedIn, unsubscribeDevice);
+router.post('/notifications/subscribe', ensureLoggedIn, subscribeDevice)
+router.post('/notifications/unsubscribe', ensureLoggedIn, unsubscribeDevice)
 
 /* GET home page. */
-router.get('/home_timeline', ensureLoggedIn, homeTimeline);
+router.get('/home_timeline', ensureLoggedIn, homeTimeline)
+
 /* GET user timeline */
 router.get('/user_timeline/:username', userTimeline)
+
 /* GET user friends and followers */
 router.get('/followers/:username', getFollowers)
 router.get('/friends/:username', getFriends)
@@ -39,37 +55,43 @@ router.get('/friends/:username', getFriends)
 router.post('/post/:postId/reply', ensureLoggedIn, replyToPost)
 
 /* GET Post liked_by and reposted_by */
-router.get('/post/:postId/likes', getLikes);
-router.get('/post/:postId/reposts', getReposts);
+router.get('/post/:postId/likes', getLikes)
+router.get('/post/:postId/reposts', getReposts)
 
 /* GET Post replies */
-router.get('/post/:postId/replies', getReplies);
+router.get('/post/:postId/replies', getReplies)
 
 /* POST create new post. */
-router.post('/post', ensureLoggedIn, createPost);
+router.post('/post', ensureLoggedIn, createPost)
 
 /* POST repost a post. */
-router.post('/repost', ensureLoggedIn, repostPost);
-/* POST unrepost a post. */
-router.post('/unrepost', ensureLoggedIn, unrepostPost);
-/* GET get a single post. */
-router.get('/post/:postId', getPost);
-router.all('/like/:postId', ensureLoggedIn, likePost);
-router.all('/unlike/:postId', ensureLoggedIn, unlikePost);
+router.post('/repost', ensureLoggedIn, repostPost)
 
+/* POST unrepost a post. */
+router.post('/unrepost', ensureLoggedIn, unrepostPost)
+
+/* GET get a single post. */
+router.get('/post/:postId', getPost)
+
+router.post('/like/:postId', ensureLoggedIn, likePost)
+router.post('/unlike/:postId', ensureLoggedIn, unlikePost)
 
 /* GET get a single user detail. */
-router.get('/user/:username', getUser);
-router.all('/follow/:username', ensureLoggedIn, followUser);
-router.all('/unfollow/:username', ensureLoggedIn, unFollowUser);
+router.get('/user/:username', getUser)
+
+router.post('/follow/:username', ensureLoggedIn, followUser)
+router.post('/unfollow/:username', ensureLoggedIn, unFollowUser)
+
 /* POST update authenticated user */
-router.post('/updateuser', ensureLoggedIn, updateUser);
+router.post('/updateuser', ensureLoggedIn, updateUser)
 
 /* GET seach results */
 router.get('/search', search)
+
 /* GET trends. */
 router.get('/trends', trends)
+
 /* GET user Suggestions */
 router.get('/users', ensureLoggedIn, userSuggests)
 
-module.exports = router;
+module.exports = router
--- routes/auth.js
+++ routes/auth.js
@@ -1,50 +1,50 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
 const passport = require('passport')
 const User = require('../models/user.model')
 const { ensureLoggedIn } = require('../utils/middlewares')
+const { filterInput } = require('../utils/helpers')
 // const { destroyAuthSession: destroySocketSession } = require('../socketApi')
+
+const router = express.Router()
 
 router.post('/login', passport.authenticate('local'), async (req, res) => {
     try {
         res.json({
             user: await User.findById(req.user._id),
-            message: 'logged in'
+            message: 'logged in',
         })
     } catch (err) {
         console.log('error in /login', err)
         res.status(500).json({
-            msg: 'Something went wrong cannot process your request right now'
+            msg: 'Something went wrong!, cannot process your request at this moment!',
         })
     }
 })
-// wait ms milliseconds
-function wait(ms) {
-    return new Promise(r => setTimeout(r, ms))
-}
+
 router.get('/login', ensureLoggedIn, async (req, res) => {
     try {
         res.json({
             user: await User.findById(req.user._id),
-            message: 'logged in'
+            message: 'logged in',
         })
     } catch (err) {
         console.log('error in get.login (checks if logged in)', err)
         res.status(500).json({
-            msg: 'Something went wrong cannot process your request right now'
+            msg: 'Something went wrong cannot process your request right now',
         })
     }
 })
-router.get('/logout', (req, res) => {
+
+router.post('/logout', (req, res) => {
     let { socketId } = req.session
     req.logout()
-    req.session.destroy((err) => {
+    req.session.destroy(err => {
         if (err) console.log('error /logout', err)
         res.redirect('/')
     })
     // destroySocketSession(socketId)
 })
-const { filterInput } = require('../utils/helpers')
+
 router.post('/signup', async (req, res) => {
     try {
         let { password, fullname, username } = req.body
@@ -53,35 +53,41 @@ router.post('/signup', async (req, res) => {
         fullname = filterInput(fullname, 'name', { min_length: 0 })
         if (await User.exists({ screen_name: username })) {
             res.status(409).json({
-                message: "username is taken"
+                message: 'username is taken',
             })
             return
         }
-        let user = await User.createNew({
-            screen_name: username,
-            name: fullname
-        }, { password })
+        let user = await User.createNew(
+            {
+                screen_name: username,
+                name: fullname,
+            },
+            { password }
+        )
         if (user)
-            req.login({
-                _id: user._id
-            }, (err) => {
-                if (err) {
-                    console.log('error logging in new user', err)
-                    res.status(409).json({
-                        message: 'user created, log in now'
+            req.login(
+                {
+                    _id: user._id,
+                },
+                err => {
+                    if (err) {
+                        console.log('error logging in new user', err)
+                        res.status(409).json({
+                            message: 'user created, log in now',
+                        })
+                        return
+                    }
+                    res.json({
+                        message: 'user succesfully created, logging in',
+                        user,
                     })
-                    return
                 }
-                res.json({
-                    message: 'user succesfully created, logging in',
-                    user
-                })
-            })
+            )
         console.log('user created', user)
     } catch (err) {
         console.log('error in /signup', err)
         res.status(400).json({
-            message: 'Your request could not be completed'
+            message: 'Your request could not be completed',
         })
     }
 })
